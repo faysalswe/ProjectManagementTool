bfbc7e6c4e31dc9fe2f1b9a376d5c5368d627bda
Faysal Ahmmad <faysal725@diu.edu.bd>
2017-11-28 13:04:30 +0600

    Add project files.

---
+++ ProjectManagementTool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectManagementTool.Startup))]
+namespace ProjectManagementTool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
