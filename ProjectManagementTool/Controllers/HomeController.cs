509d17b2d817532451bfbef35e58c1419d54cd36
Faysal Ahmmad <faysal725@diu.edu.bd>
2017-12-04 05:08:58 +0600

    added change password functionality and update database files

--- ProjectManagementTool/Controllers/HomeController.cs
+++ ProjectManagementTool/Controllers/HomeController.cs
@@ -32,48 +32,6 @@ namespace ProjectManagementTool.Controllers
             {
                 return RedirectToAction("Index", "Tasks");
             }
-
-        }
-
-        //[HttpPost]
-        //public ActionResult Index(Project project)
-        //{
-        //    //Directory.CreateDirectory(Server.MapPath("~/Content/ProjectFile" + "\\" + project.Name));
-
-        //    //foreach (var file in project.Files)
-        //    //{
-        //    //    if (file.ContentLength > 0)
-        //    //    {
-        //    //        var fileName = Path.GetFileName(file.FileName);
-        //    //        var path = Path.Combine(Server.MapPath("~/Content/ProjectFile/"+ project.Name), fileName);
-        //    //        file.SaveAs(path);
-        //    //    }
-
-        //    //}
-        //    return View();
-        //}
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "About Page";
-            //string path = Server.MapPath("~/Content/ProjectFile/VVC");
-            //List<Content> Contents = new List<Content>();
-            //foreach (var files in Directory.GetFiles(path))
-            //{ 
-            //    FileInfo info = new FileInfo(files);
-            //    Content content = new Content();
-            //    content.Path = "~/Content/ProjectFile/VVC/" + Path.GetFileName(info.FullName);
-            //    content.Name = Path.GetFileName(info.FullName);
-            //    Contents.Add(content);
-            //}
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
